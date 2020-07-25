d0331d4098c65e4f82c7ffc805bc9ed3aea7e5b6
github Koichi04 <enrichlife0429@gmail.com>
2020-07-26 01:58:14 +0900

    深夜のコミット

--- lib/home_route.dart
+++ lib/home_route.dart
@@ -1,4 +1,6 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class Home extends StatelessWidget {
   @override
@@ -8,11 +10,14 @@ class Home extends StatelessWidget {
           title: Text('タイムライン'),
           actions: <Widget>[
             IconButton(
-              icon: Icon(Icons.add_circle_outline, color: Colors.white,),
+              icon: Icon(
+                Icons.add_circle_outline,
+                color: Colors.white,
+              ),
               onPressed: () {
                 Navigator.push(
                   context,
-                  MaterialPageRoute(builder: (context) => SecondRoute()),
+                  MaterialPageRoute(builder: (context) => PickPhoto()),
                 );
               },
             ),
@@ -20,55 +25,73 @@ class Home extends StatelessWidget {
         ),
         body: ListView(
             children: List.generate(3, (index) {
-              return InkWell(
-                onTap: (){
-                  Navigator.push(context, MaterialPageRoute(builder: (context) => MyHomePageDetail("images/$index.jpg")));
-                },
-                child: Card(
-                  child: Column(
-                    children: <Widget>[
-                      Image.asset("images/$index.jpg"),
-                      Container(
-                          margin: EdgeInsets.all(10.0),
-                          child: ListTile(
-                            title: Text("$index.jpg"),
-                            leading: Icon(Icons.person),
-                            subtitle: Text("サブタイトル"),
-                          )),
-                    ],
-                  ),
-                ),
-              );
-            }))
-    );
+          return InkWell(
+            onTap: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) =>
+                          MyHomePageDetail("images/$index.jpg")));
+            },
+            child: Card(
+              child: Column(
+                children: <Widget>[
+                  Image.asset("images/$index.jpg"),
+                  Container(
+                      margin: EdgeInsets.all(10.0),
+                      child: ListTile(
+                        title: Text("$index.jpg"),
+                        leading: Icon(Icons.person),
+                        subtitle: Text("サブタイトル"),
+                      )),
+                ],
+              ),
+            ),
+          );
+        })));
   }
 }
-class SecondRoute extends StatelessWidget {
+
+class PickPhoto extends StatefulWidget {
+  @override
+  _PickPhotoState createState() => _PickPhotoState();
+}
+
+class _PickPhotoState extends State<PickPhoto> {
+  File _image;
+  final picker = ImagePicker();
+
+  Future getImage() async {
+    final pickedFile = await picker.getImage(source: ImageSource.gallery);
+
+    setState(() {
+      _image = File(pickedFile.path);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("投稿画面"),
+        title: Text('投稿画面'),
       ),
       body: Center(
         child: Column(
           children: <Widget>[
-            Image.asset(
-              'images/noteImage.jpg',
-            ),
-            FlatButton(
-              onPressed: (){
-                //TODO Upload image from library
-
-              },
-              child: Icon(
-                  Icons.photo_camera
-              ),
-            ),
+            _image == null
+                ? Padding(
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 100.0, horizontal: 50.0),
+                    child: Text('写真を選択してください'),
+                  )
+                : Image.file(_image),
             Padding(
-              padding: const EdgeInsets.all(30.0),
+              padding:
+                  const EdgeInsets.symmetric(vertical: 10.0, horizontal: 50.0),
               child: TextField(
-                enabled: true,
+                decoration: InputDecoration(
+                  labelText: "概要など",
+                ),
               ),
             ),
             RaisedButton(
@@ -81,32 +104,20 @@ class SecondRoute extends StatelessWidget {
           ],
         ),
       ),
-    );
-  }
-}
-class ThirdRoute extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Third Route"),
-      ),
-      body: Center(
-        child: RaisedButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          child: Text('Go second'),
-        ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: getImage,
+        tooltip: 'Pick Image',
+        child: Icon(Icons.add_a_photo),
       ),
     );
   }
 }
-class MyHomePage extends StatefulWidget {
 
+class MyHomePage extends StatefulWidget {
   @override
   _MyHomePageState createState() => new _MyHomePageState();
 }
+
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
@@ -116,34 +127,41 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
         body: ListView(
             children: List.generate(3, (index) {
-              return InkWell(
-                onTap: (){
-                  Navigator.push(context, MaterialPageRoute(builder: (context) => MyHomePageDetail("images/$index.jpg")));
-                },
-                child: Card(
-                  child: Column(
-                    children: <Widget>[
-                      Image.asset("images/$index.jpg"),
-                      Container(
-                          margin: EdgeInsets.all(10.0),
-                          child: ListTile(
-                            title: Text("$index.jpg"),
-                            leading: Icon(Icons.person),
-                            subtitle: Text("サブタイトル"),
-                          )),
-                    ],
-                  ),
-                ),
-              );
-            })));
+          return InkWell(
+            onTap: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) =>
+                          MyHomePageDetail("images/$index.jpg")));
+            },
+            child: Card(
+              child: Column(
+                children: <Widget>[
+                  Image.asset("images/$index.jpg"),
+                  Container(
+                      margin: EdgeInsets.all(10.0),
+                      child: ListTile(
+                        title: Text("$index.jpg"),
+                        leading: Icon(Icons.person),
+                        subtitle: Text("サブタイトル"),
+                      )),
+                ],
+              ),
+            ),
+          );
+        })));
   }
 }
+
 class MyHomePageDetail extends StatefulWidget {
   MyHomePageDetail(this._imageName);
   final String _imageName;
   @override
-  _MyHomePageDetailState createState() => new _MyHomePageDetailState(_imageName);
+  _MyHomePageDetailState createState() =>
+      new _MyHomePageDetailState(_imageName);
 }
+
 class _MyHomePageDetailState extends State<MyHomePageDetail> {
   _MyHomePageDetailState(this._imageName);
   final String _imageName;

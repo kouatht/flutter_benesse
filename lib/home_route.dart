68a32e161a4d5a316bdf83035d85c09c1f6cd865
KarenShichijo <karen7jo@icloud.com>
2020-07-25 22:08:33 +0900

    投稿画面の作成

--- lib/home_route.dart
+++ lib/home_route.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -31,14 +33,34 @@ class SecondRoute extends StatelessWidget {
         title: Text("投稿画面"),
       ),
       body: Center(
-        child: RaisedButton(
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => ThirdRoute()),
-            );
-          },
-          child: Text('次の画面'),
+        child: Column(
+          children: <Widget>[
+            Image.asset(
+              'images/noteImage.jpg',
+            ),
+            FlatButton(
+              onPressed: (){
+                //TODO Upload image from library
+
+              },
+              child: Icon(
+                Icons.photo_camera
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(30.0),
+              child: TextField(
+                enabled: true,
+              ),
+            ),
+            RaisedButton(
+              onPressed: () {
+                //TODO Save command
+                Navigator.pop(context);
+              },
+              child: Text('保存'),
+            ),
+          ],
         ),
       ),
     );
